8160ca1d036e475f28e48fb2c4304a15864eec50
Dzelo <adzelo2@etf.unsa.ba>
2024-10-23 13:49:52 +0200

    Testiranje kolekcija

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,49 @@
+
+import java.util.*;
+
+import static java.util.Collections.sort;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+//        Date datum = new Date(2018, 8, 2);
+//        Date datum2 = datum;
+//        datum2.setDate(21);
+//        System.out.println(datum);
+//        double x=15.6167;
+//        System.out.println(Math.round(x));
+//        System.out.println(Math.floor(x));
+//        System.out.println(Math.ceil(x));
+//        String s = new String("Hello World");
+//        s+= x;
+//        System.out.println(s);
+//        double[] temp = new double[10];
+        ArrayList<Integer> brojevi = new ArrayList<Integer>();
+        brojevi.add(1);
+        brojevi.add(1);
+        brojevi.add(1);
+        brojevi.add(1);
+        brojevi.add(1);
+        brojevi.add(0,6);
+        brojevi.add(1,3);
+        System.out.println(brojevi.get(2));
+        Iterator<Integer> it= brojevi.iterator();
+        int b = it.next();
+        System.out.println(b);
+         b = it.next();
+        System.out.println(b);
+         b = it.next();
+        System.out.println(b);
+        sort(brojevi);
+        System.out.println(brojevi);
+        brojevi.sort(Collections.reverseOrder());
+        System.out.println(brojevi);
 
-        System.out.print("Hello and welcome!");
-        System.out.print("Hello and welcome!");
-        System.out.print("Hello and welcome!");
-        //ala sam kralj
-        System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
+
     }
 }
+
