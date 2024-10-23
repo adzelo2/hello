2cdaa2538ed6c6b78e0ff8a020b0b04feda8ba36
Dzelo <adzelo2@etf.unsa.ba>
2024-10-23 13:51:26 +0200

    Testiranje kolekcija

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,20 @@ import static java.util.Collections.sort;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        Date datum = new Date(2018, 8, 2);
-//        Date datum2 = datum;
-//        datum2.setDate(21);
-//        System.out.println(datum);
-//        double x=15.6167;
-//        System.out.println(Math.round(x));
-//        System.out.println(Math.floor(x));
-//        System.out.println(Math.ceil(x));
-//        String s = new String("Hello World");
-//        s+= x;
-//        System.out.println(s);
-//        double[] temp = new double[10];
+                Date datum = new Date(2018, 8, 2);
+        Date datum2 = datum;
+        datum2.setDate(21);
+        System.out.println(datum);
+        double x=15.6167;
+        System.out.println(Math.round(x));
+        System.out.println(Math.floor(x));
+        System.out.println(Math.ceil(x));
+        String s = new String("Hello World");
+        s+= x;
+        System.out.println(s);
+        double[] temp = new double[10];
+        //-----------------------------------
+        //----------------------
         ArrayList<Integer> brojevi = new ArrayList<Integer>();
         brojevi.add(1);
         brojevi.add(1);
